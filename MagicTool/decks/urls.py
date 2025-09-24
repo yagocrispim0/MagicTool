6d6b2bc1e0859c70824017a82d6f58a197e47248
yagocrispim0 <yagocrgomes@gmail.com>
2025-09-24 02:50:13 -0300

    Pagina de listagem de decks

--- MagicTool/decks/urls.py
+++ MagicTool/decks/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #URL para listar decks criados
+    path('', views.decks_list, name='decks_list'),
+    #URL para criar um novo deck
     path('create/', views.create_deck, name = 'create_deck'),
+
 ]
--- MagicTool/decks/views.py
+++ MagicTool/decks/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Deck
 
 # Create your views here.
 def create_deck(request):
+
+    if request.method == 'POST':
+        deck_name = request.POST.get('deck_name')
+        deck_format = request.POST.get('deck_format')
+
+        Deck.objects.create(name=deck_name, format=deck_format)
+
+        return  redirect('decks_list')
+    
     return render(request, 'decks/create_deck.html')
+
+
+def decks_list(request):
+
+    all_decks = Deck.objects.order_by('name')
+    context = {'decks': all_decks}
+    return render(request, 'decks/decks_list.html', context)
