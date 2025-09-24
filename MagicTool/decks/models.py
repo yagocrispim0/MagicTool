ed415edb1e7d41396011d6eb0ac1fde67ae26593
yagocrispim0 <yagocrgomes@gmail.com>
2025-09-24 02:23:08 -0300

    View - Create Deck

--- MagicTool/decks/models.py
+++ MagicTool/decks/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 
 class Deck(models.Model):
+    # Defines format choices for the deck
+    class FormatChoices(models.TextChoices):
+        STANDARD = 'STD', 'Standard'
+        MODERN = 'MOD', 'Modern'
+        LEGACY = 'LGC', 'Legacy'
+        VINTAGE = 'VIN', 'Vintage'
+        COMMANDER = 'EDH', 'Commander'
+        PAUPER = 'PPR', 'Pauper'
+        NONE = 'NON', 'Sem Formato'
+
     name = models.CharField(max_length=200)
-    cards = models.ManyToManyField(Card)
+    format = models.CharField(max_length=3, choices=FormatChoices.choices, default=FormatChoices.NONE)
+    cards = models.ManyToManyField('Card')
 
     def __str__(self):
         return self.name
--- MagicTool/decks/views.py
+++ MagicTool/decks/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_deck(request):
+    return render(request, 'decks/create_deck.html')
